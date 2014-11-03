c5ede617881bb95f874a54fdecc0b1425e6faf63
Daniel Huie <huied93@uw.edu>
2014-11-03 12:09:19 -0800

    class angularjs assignment

--- js/app.js
+++ js/app.js
@@ -5,3 +5,23 @@
 
 "use strict";
 
+// create new angular module, all controllers attach to a module; eliminates need for global variables
+angular.module('MovieApp', [])
+    // when angular sees a $ associated variable, it injects it to the controller
+    // $scope = what your view will be able to see; connects view and controller to manipulate the specified variable
+    .controller('MoviesController', function($scope) {
+        $scope.movies = movies;
+        $scope.searchString = '';
+        $scope.sortCol = 'rank';
+        $scope.sortReverse = false;
+
+        $scope.sortBy = function(colName) {
+            if ($scope.sortCol == colName) {
+                $scope.sortReverse = !$scope.sortReverse;
+            } else {
+                $scope.sortReverse = false;
+            }
+            $scope.sortCol = colName;
+        }
+    });
+
